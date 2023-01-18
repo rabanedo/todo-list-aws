8d3fb8ad8be5fc48bebc3d56d9cd3f6482b93bba
rabanedo <122230847+rabanedo@users.noreply.github.com>
2023-01-18 17:48:40 +0100

    Increase proof of coverage

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -47,7 +47,7 @@ class TestDatabaseFunctions(unittest.TestCase):
     def test_table_exists(self):
         print ('---------------------')
         print ('Start: test_table_exists')
-        #self.assertTrue(self.table)  # check if we got a result
+        self.assertTrue(self.table)  # check if we got a result
         #self.assertTrue(self.table_local)  # check if we got a result
 
         print('Table name:' + self.table.name)
@@ -68,8 +68,8 @@ class TestDatabaseFunctions(unittest.TestCase):
         print ('Response put_item:' + str(response))
         self.assertEqual(200, response['statusCode'])
         # Table mock
-        #self.assertEqual(200, put_item(self.text, self.dynamodb)[
-        #                 'ResponseMetadata']['HTTPStatusCode'])
+        self.assertEqual(200, put_item(self.text, self.dynamodb)[
+                         'ResponseMetadata']['HTTPStatusCode'])
         print ('End: test_put_todo')
 
     def test_put_todo_error(self):
