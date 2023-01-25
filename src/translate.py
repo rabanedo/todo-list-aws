d0d01d668bf3b0c63071beda3e2ba17bd17fd704
rabanedo <122230847+rabanedo@users.noreply.github.com>
2023-01-26 00:54:17 +0100

    Fix translate

--- src/translate.py
+++ src/translate.py
@@ -6,11 +6,13 @@ import todoList
 def translate(event, context):
     # create a response
     item = todoList.get_item(event['pathParameters']['id'])
-    translation = todoList.translate_item(item['text'], event['pathParameters']['language'])
+    translation = todoList.translate_item(
+        item['text'], event['pathParameters']['language'])
     if translation:
         response = {
             "statusCode": 200,
-            "body": json.dumps(translation, cls=decimalencoder.DecimalEncoder)
+            "body": json.dumps(result,
+                               cls=decimalencoder.DecimalEncoder)
         }
     else:
         response = {
