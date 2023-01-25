ea8ca484fac0e28cde4809a94a1ac0d975411254
rabanedo <122230847+rabanedo@users.noreply.github.com>
2023-01-26 00:46:30 +0100

    Fix translate function

--- src/translate.py
+++ src/translate.py
@@ -1,16 +1,16 @@
 import json
+import decimalencoder
 import todoList
 
 
 def translate(event, context):
     # create a response
     item = todoList.get_item(event['pathParameters']['id'])
-    translation = todoList.translate_item(
-        item['text'], event['pathParameters']['language'])
+    translation = todoList.translate_item(item['text'], event['pathParameters']['language'])
     if translation:
         response = {
             "statusCode": 200,
-            "body": json.dumps(translation)
+            "body": json.dumps(translation, cls=decimalencoder.DecimalEncoder)
         }
     else:
         response = {
