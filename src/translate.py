dbb7605140b841373c92547628f40f6ef34ddf59
rabanedo <122230847+rabanedo@users.noreply.github.com>
2023-02-02 19:04:45 +0100

    Modify translate

--- src/translate.py
+++ src/translate.py
@@ -5,21 +5,15 @@ import todoList
 
 def translate(event, context):
     # create a response
-    item = todoList.get_item(event['pathParameters']['id'])
     print("##### Parámetros ##### \n\r")
     print(json.dumps(event['pathParameters'], indent=4))
     translation = str(
         todoList.get_translate(
-            item['text'], event['pathParameters']['language']
+            event['pathParameters']['id'], 
+            event['pathParameters']['language']
         )
     )
-    item["text"] = translation
     print("##### Traducción ##### \n\r")
-    print(
-        json.dumps(
-            item["text"], cls=decimalencoder.DecimalEncoder, indent=4
-        )
-    )
     if translation:
         response = {
             "statusCode": 200,
